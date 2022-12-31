61e8fe0a0d280b87a317ff4bd6bbd3df1a80d969
rayan <rayanzyad99@gmail.com>
2022-12-31 04:12:50 +0000

    How to write

---
+++ src/main/java/main.java
@@ -0,0 +1,19 @@
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class main {
+    public static void main(String[] args) {
+     File myfile = new File("MyFile2.txt");
+        System.out.println("My file is located at " + myfile.getAbsolutePath());
+        String content = "Details to write";
+        try{
+            FileWriter myWriter = new FileWriter(myfile);
+            myWriter.write("ggg");
+            myWriter.close();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
